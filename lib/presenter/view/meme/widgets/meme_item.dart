d2bb8adc7c7030842f6ce44bcdb74e73ed95c449
Pras <prasetyaarfanto@gmail.com>
2023-04-17 12:30:25 +0700

    Add meme_item.dart widget

---
+++ lib/presenter/view/meme/widgets/meme_item.dart
@@ -0,0 +1,21 @@
+import 'package:cached_network_image/cached_network_image.dart';
+import 'package:flutter/material.dart';
+import 'package:mim_generator/data/entity/meme.dart';
+
+class MemeItem extends StatelessWidget {
+  final Meme _meme;
+
+  const MemeItem(this._meme, {super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: CachedNetworkImage(
+          fit: BoxFit.fill,
+          imageUrl: _meme.url ?? "",
+          progressIndicatorBuilder: (context, url, downloadProgress) =>
+              CircularProgressIndicator(value: downloadProgress.progress),
+          errorWidget: (context, url, error) => const Icon(Icons.error)),
+    );
+  }
+}
