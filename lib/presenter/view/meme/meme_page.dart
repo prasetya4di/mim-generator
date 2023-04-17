83dad3e0f0649589f8d9075b17b92ebb7cac391b
Pras <prasetyaarfanto@gmail.com>
2023-04-17 11:49:52 +0700

    Add meme page

---
+++ lib/presenter/view/meme/meme_page.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class MemePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+      body: Text("Pras nih bos"),
+    );
+  }
+}
