44c1871c25d472114d19ac4c7dfd541b86e06fef
Pras <prasetyaarfanto@gmail.com>
2023-04-17 12:30:54 +0700

    Update meme_page to show all available meme

--- lib/presenter/view/meme/meme_page.dart
+++ lib/presenter/view/meme/meme_page.dart
@@ -1,11 +1,29 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:mim_generator/presenter/view/meme/bloc/meme_bloc.dart';
+import 'package:mim_generator/presenter/view/meme/bloc/meme_state.dart';
+import 'package:mim_generator/presenter/view/meme/widgets/list_meme.dart';
 
 class MemePage extends StatelessWidget {
+  const MemePage({super.key});
+
   @override
   Widget build(BuildContext context) {
+    MemeBloc bloc = context.read();
+
+    WidgetsFlutterBinding.ensureInitialized().addPostFrameCallback((timeStamp) {
+      bloc.add(const MemeEvent.loadMeme());
+    });
+
     return Scaffold(
-      appBar: AppBar(),
-      body: Text("Pras nih bos"),
+      appBar: AppBar(
+        title: const Text("Mim Generator"),
+      ),
+      body: BlocBuilder<MemeBloc, MemeState>(
+        builder: (context, state) {
+          return ListMeme(bloc.memes);
+        },
+      ),
     );
   }
 }
