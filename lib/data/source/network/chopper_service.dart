d593e1670afc1f69004e7e36449aaef56aa08893
Pras <prasetyaarfanto@gmail.com>
2023-04-17 11:04:44 +0700

    Add meme service to chopper service

--- lib/data/source/network/chopper_service.dart
+++ lib/data/source/network/chopper_service.dart
@@ -1,9 +1,10 @@
 import 'package:chopper/chopper.dart';
+import 'package:mim_generator/data/source/network/meme_service.dart';
 import 'package:mim_generator/util/converter/response_converter.dart';
 
 ChopperClient chopperClient() => ChopperClient(
       baseUrl: Uri.parse("https://api.imgflip.com"),
       converter: ResponseConverter(),
       errorConverter: ResponseConverter(),
-      services: [],
+      services: [MemeService.create()],
     );
--- lib/data/source/network/meme_service.dart
+++ lib/data/source/network/meme_service.dart
@@ -5,6 +5,8 @@ part 'meme_service.chopper.dart';
 
 @ChopperApi()
 abstract class MemeService extends ChopperService {
+  static MemeService create([ChopperClient? client]) => _$MemeService(client);
+
   @Get(path: "/get_memes")
   Future<Response<MemeResponse>> getMeme();
 }
